1d046e6d31aee00c80234418a795cb48071a4ff4
Rodrigo Santelices <rcsantelices@gmail.com>
2018-05-02 22:50:22 -0400

    travis test

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 'use strict';
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/restaurants-app';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://rod:Test@ds113700.mlab.com:13700/restaurant-ci';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-restaurants-app';
 exports.PORT = process.env.PORT || 8080;
